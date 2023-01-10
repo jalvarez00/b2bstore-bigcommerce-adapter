d85de84423cc7fa857ed2e1dd0fab26d23124520
jalvarez00 <jalvarez@orienteed.com>
2023-01-10 09:02:26 +0100

    modify request

--- src/Products/getProducts.gql.ts
+++ src/Products/getProducts.gql.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client"
 
 export const GET_PRODUCTS = gql`
   query GetProducts {
-    products(search: "ATTIC") {
+    products(search: "QUATRO") {
       items {
         uid
         name
